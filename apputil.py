17140efe08cdaced84ef937094912b3d1f013ca1
Josh Gaughan <jdgaugha@iu.edu>
2025-11-07 16:08:23 +0000

    kmeans_diamonds() comments and docstring.

--- apputil.py
+++ apputil.py
@@ -55,12 +55,13 @@ def kmeans_diamonds(n, k):
     a 2D array of the cluster centroids and a 1D array
     of the index of the assigned cluster for each row in X.
     '''
-    # Define a subset containing only the 
-    #subset = diamonds.head(n)
-    subset = numerical_cols.head(n)
+    # Define a subset containing only the n rows.
+    subset = diamonds.head(n)
 
-    X = subset.values
+    # Define the array that goes in the kmeans function.
+    X = subset[numerical_cols].values
 
+    # Define the centroids and labels using the kmeans function.
     centroids, labels = kmeans(X, k)
 
     return (centroids, labels)
@@ -82,3 +83,29 @@ def kmeans_timer(n, k, n_iter = 5):
         runtimes.append(duration)
 
         return sum(runtimes) / len(runtimes)
+
+# Exercise output that is required for this week
+
+from apputil import *
+
+%config InlineBackend.figure_formats = ['svg']
+sns.set_theme(style="whitegrid")
+
+n_values = np.arange(100, 50000, 1000)
+k5_times = [kmeans_timer(n, 5, 20) for n in n_values]
+
+k_values = np.arange(2, 50)
+n10k_times = [kmeans_timer(10000, k, 10) for k in k_values]
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 5), sharey=True)
+fig.tight_layout()
+fig.suptitle("KMeans Time Complexity", y=1.08, fontsize=14)
+
+sns.lineplot(x=n_values, y=k5_times, ax=axes[0])
+axes[0].set_xlabel("Number of Rows (n)")
+axes[0].set_ylabel("Time (seconds)")
+axes[0].set_title('Increasing n for k=5 Clusters')
+
+sns.lineplot(x=k_values, y=n10k_times, ax=axes[1])
+axes[1].set_xlabel("Number of Clusters (k)")
+axes[1].set_title('Increasing k for n=10k Samples');
