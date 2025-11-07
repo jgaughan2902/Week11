1443fcd86304d33baeb16ee2aece3a7b7620a2ba
Josh Gaughan <jdgaugha@iu.edu>
2025-11-07 16:21:39 +0000

    kmeans_timer() comments and docstring.

--- apputil.py
+++ apputil.py
@@ -69,43 +69,44 @@ def kmeans_diamonds(n, k):
 centroids, labels = kmeans_diamonds(n=1000, k=5)
 
 def kmeans_timer(n, k, n_iter = 5):
+    '''
+    Runs the kmeans_diamonds() function and saves
+    the runtime for each run.
+
+    Parameters:
+    n (int): The desired amount of rows for the 
+    kmeans_diamonds() function.
+    k (int): The number of clusters you want for the
+    kmeans_diamonds() function
+    n_iter (int): The amount of times you want the
+    kmeans_diamonds() function to run.
+
+    Return value:
+    sum(runtimes) / len(runtimes): The average time
+    across the n runs (in seconds).
+    '''
+    
+    # Initiate a blank list for runtimes
     runtimes = []
 
-    for i in range(n_iter):
+    # Run a dummy call to improve consistency.
+    if n_iter > 0:
+            _ = kmeans_diamonds(10, 2)
+
+    # Start a for loop for the amount of iterations.
+    for _ in range(n_iter):
+        
+        # Store a beginning time for the iteration.
         start_time = time.time()
 
+        # Run the k_means function (though we don't care
+        # about the output in this case).
         _ = kmeans_diamonds(n, k)
 
-        end_time = time.time()
+        # Record the duration of the iteration.
+        duration = time.time() - start_time
 
-        duration = end_time - start_time
-
+        # Append it to the runtimes list.
         runtimes.append(duration)
 
-        return sum(runtimes) / len(runtimes)
-
-# Exercise output that is required for this week
-
-from apputil import *
-
-%config InlineBackend.figure_formats = ['svg']
-sns.set_theme(style="whitegrid")
-
-n_values = np.arange(100, 50000, 1000)
-k5_times = [kmeans_timer(n, 5, 20) for n in n_values]
-
-k_values = np.arange(2, 50)
-n10k_times = [kmeans_timer(10000, k, 10) for k in k_values]
-
-fig, axes = plt.subplots(1, 2, figsize=(10, 5), sharey=True)
-fig.tight_layout()
-fig.suptitle("KMeans Time Complexity", y=1.08, fontsize=14)
-
-sns.lineplot(x=n_values, y=k5_times, ax=axes[0])
-axes[0].set_xlabel("Number of Rows (n)")
-axes[0].set_ylabel("Time (seconds)")
-axes[0].set_title('Increasing n for k=5 Clusters')
-
-sns.lineplot(x=k_values, y=n10k_times, ax=axes[1])
-axes[1].set_xlabel("Number of Clusters (k)")
-axes[1].set_title('Increasing k for n=10k Samples');
+    return sum(runtimes) / len(runtimes)
